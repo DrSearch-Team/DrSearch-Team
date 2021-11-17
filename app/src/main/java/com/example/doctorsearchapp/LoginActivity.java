af7f1f67776546b02da66b1ea2d1489c4a197402
Conspiciens <fernandezjohann08@gmail.com>
2021-11-16 21:48:15 -0800

    Fixed issues with merge

--- app/src/main/java/com/example/doctorsearchapp/LoginActivity.java
+++ app/src/main/java/com/example/doctorsearchapp/LoginActivity.java
@@ -63,7 +63,7 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
                 // TODO: Navigate to the main activity if the user has signed in properly
-                // goMainActivity();
+                goMainActivity();
                 Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
             }
         });
