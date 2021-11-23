2783cca8fc0534c3b2775415cb72b63d1725143e
Kania Ganda <kaniagandacs@gmail.com>
2021-11-22 23:45:42 -0800

    Added functions in doctor model

--- app/src/main/java/com/example/doctorsearchapp/models/Doctor.java
+++ app/src/main/java/com/example/doctorsearchapp/models/Doctor.java
@@ -9,4 +9,54 @@ public class Doctor {
     public int overallRating;
     public String location;
     public ArrayList<Integer> reviewList;
+
+    public Doctor() {};
+
+    public Doctor(int doctorId, String doctorName, int overallRating, String location, ArrayList<Integer> reviewList) {
+        this.doctorId = doctorId;
+        this.doctorName = doctorName;
+        this.overallRating = overallRating;
+        this.location = location;
+        this.reviewList = reviewList;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+
+    public int getOverallRating() {
+        return overallRating;
+    }
+
+    public void setOverallRating(int overallRating) {
+        this.overallRating = overallRating;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public ArrayList<Integer> getReviewList() {
+        return reviewList;
+    }
+
+    public void setReviewList(ArrayList<Integer> reviewList) {
+        this.reviewList = reviewList;
+    }
 }
